6fa68a38750274c8cb13f8375e9d6471a13d0f44
Allen Trinh <allenmtrinh@gmail.com>
2022-01-28 21:10:51 -0500

    Feature: add Toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps, computed } from "vue";
+
+const props = defineProps({
+  message: String,
+  type: {
+    type: String,
+    validator: (value) => {
+      return ["danger", "warning", "info", "success"].includes(value);
+    },
+  },
+  visible: Boolean,
+});
+
+const colors = computed(() => {
+  switch (props.type) {
+    case "danger":
+      return "bg-red-400 text-red-900";
+
+    case "success":
+      return "bg-green-400 text-black";
+  }
+});
+
+const visibility = computed(() => {
+  return props.visible ? "opacity-100" : "opacity-0";
+});
+</script>
+
+<template>
+  <div class="rounded py-2 px-6 mb-6 fixed top-4 transition-all" :class="[colors, visibility]">
+    {{ props.message }}
+  </div>
+</template>
